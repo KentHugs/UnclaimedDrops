11c8f675e43e9678b1de1a5e38aad917cabd5af8
!Kent <48135150+KentHugs@users.noreply.github.com>
2023-07-07 18:56:07 +0300

    Update data3.js

--- docs/data3.js
+++ docs/data3.js
@@ -1,3 +1,3 @@
 const DATA3 = [
-  { address: "0xdeea37565a2fbcacb99d0e11e3080593b96cb54b", value: "12995,8725", notes: "Unclaimed" },
+  { address: "0xdeea37565a2fbcacb99d0e11e3080593b96cb54b", value: "", notes: "" },
 ];
