00e83bf8dde8ae3f3186096dc886d5055390ff36
!Kent <48135150+KentHugs@users.noreply.github.com>
2023-07-07 18:40:55 +0300

    Update data2.js

--- docs/data2.js
+++ docs/data2.js
@@ -1,3 +1,10 @@
 const DATA2 = [
-  { address: "0x2222222222222222222222222222222222222222", value: "0.02 ABC", notes: "Test data2" },
+  { address: "0x134b58a2854cd11cd31f1ae270d52bb4ee018b4f", value: "372.07", notes: null },
+{ address: "0x22dff26cb5fd9b158793056c019762ac7142529e", value: "559.47", notes: null },
+{ address: "0xfc5a91830b1bdf893dde36be1086d22bc033ee81", value: "372.07", notes: null },
+{ address: "0x83be2dea749f412ecd2e302291c696f09b49d232", value: "8875.23", notes: null },
+{ address: "0xb1295a038a00b48c72ce73fcd1cc8b50f139b069", value: "1426.38", notes: null },
+{ address: "0x2dadc441e98ac3f2103a47046dfd6c342b89b392", value: "1584.86", notes: null },
+{ address: "0x209bcff009b6d45e3b6cf7cfae17753d8d54ef88", value: "4120.64", notes: null },
+{ address: "0x5e81c39e255bcce6bc0a8985b7c98bfd567ec1df", value: "1062.85", notes: null },
 ];
